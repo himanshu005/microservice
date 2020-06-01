8a0934821dbdca5dbb20583fc6a0e67a586b3e9e
Himanshu Sawarkar <>
2020-06-01 19:08:29 +0530

    gres full shutdown added

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/himanshu005/microservice
+
+go 1.14
+
+require golang.org/x/net v0.0.0-20200528225125-3c3fba18258b // indirect
--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/himanshu005/microservice/handler"
+	"golang.org/x/net/context"
 )
 
 func main() {
-	fmt.Println("Hello word")
+	l := log.New(os.Stdout, "product-api :", log.LstdFlags)
+	hh := handler.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("Recieved terminate,gracefull shoutdown", sig)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
